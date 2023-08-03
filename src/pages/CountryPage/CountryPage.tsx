e39c600602e8ca31694d54554e957717db13cb50
Glonuel342 <gloria.gyamfi@amalitech.com>
2023-08-03 16:26:58 -0700

    added some implementation to routes and pages  ts

--- src/pages/CountryPage/CountryPage.tsx
+++ src/pages/CountryPage/CountryPage.tsx
@@ -6,26 +6,60 @@ import { CountryTS } from '../../types/Country';
 import { useApi } from '../../contexts/ApiContext';
 import { useForm } from '../../contexts/ThemeContext';
 
-export const CountryPage = () =>{
-    const { state } = useForm();
-    const { name } = useParams();
-    const countries =useApi();
+export const CountryPage = () => {
+  const { state } = useForm();
+  const { name } = useParams();
+  const countries = useApi();
 
-    const [loading,setLoading] = useState();
-    const [country, setCountry] = useState<CountryTS[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [country, setCountry] = useState<CountryTS[]>([]);
 
-   useEffect(() =>{
+  useEffect(() => {
     if (name) {
-        getCountry(name);
+      getCountry(name);
     }
-   },[name]);
+  }, [name]);  
 
-   const getCountry = async (param: string) => {
-    setLoading
-    
-   
-    };
-   };
+  const getCountry = async (param: string) => {
+    setLoading(true);
+    try {
+      let country = countries.find((item: CountryTS) => item.name === param);
+      setCountry(country ? [country] : []);
+    } catch (error) {
+      console.error('Error fetching country:', error);
+      setCountry([]);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <C.CountryPage theme={state.theme}>
+      <div className='container'>
+        <Link to="/" className='back--button'><i className="fas fa-long-arrow-alt-left"></i> Back</Link>
+        {loading &&
+          <div className='loading'>Loading...</div>
+        }
+        {!loading && country.map((item) => (
+          <SingleCountry
+            key={item.name}
+            flag={item.flags.png}
+            name={item.name}
+            nativeName={item.nativeName}
+            topLevelDomain={item.topLevelDomain[0]}
+            population={item.population}
+            currencies={item.currencies && item.currencies}
+            region={item.region}
+            languages={item.languages}
+            subregion={item.subregion}
+            capital={item.capital}
+            borders={item.borders}
+          />
+        ))}
+      </div>
+    </C.CountryPage>
+  );
+};
 
    
 
